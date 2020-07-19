68b48e30d0fa2198b9b7349b7af7b1e8c04a3402
D7682 <danielgomez7682@gmail.com>
2020-07-18 20:38:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
